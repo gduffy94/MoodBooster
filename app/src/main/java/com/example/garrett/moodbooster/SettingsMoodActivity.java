96729cb0ef95263fdef37622553251e1f9bfa6c7
Brittany Harrington <bharrin4@nd.edu>
2017-04-12 10:47:25 -0400

    Current state of preferences

--- app/src/main/java/com/example/garrett/moodbooster/SettingsMoodActivity.java
+++ app/src/main/java/com/example/garrett/moodbooster/SettingsMoodActivity.java
@@ -1,6 +1,7 @@
 package com.example.garrett.moodbooster;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,6 +10,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 /**
  * Created by User on 4/11/2017.
@@ -16,7 +19,7 @@ import android.widget.ImageView;
 
 public class SettingsMoodActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageButton sad_puppy;
+    private ImageButton sad_panda;
     private ImageButton sad_music;
     private ImageButton sad_nature;
     private ImageButton sad_recipes;
@@ -29,41 +32,94 @@ public class SettingsMoodActivity extends AppCompatActivity implements View.OnCl
     private ImageButton sad_travel;
     private ImageButton sad_funny;
 
+    private ImageButton nextArrow;
+    private ImageButton backArrow;
+
+    int page = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mood_settings);
+        setContentView(R.layout.activity_preferences);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("MoodBooster");
 
-        sad_puppy = (ImageButton) findViewById(R.id.sad_panda);
-        sad_music = (ImageButton) findViewById(R.id.sad_music);
-        sad_nature = (ImageButton) findViewById(R.id.sad_nature);
-        sad_recipes = (ImageButton) findViewById(R.id.sad_recipes);
-        sad_exercise = (ImageButton) findViewById(R.id.sad_exercise);
-        sad_family = (ImageButton) findViewById(R.id.sad_family);
-        sad_friends = (ImageButton) findViewById(R.id.sad_friends);
-        sad_books = (ImageButton) findViewById(R.id.sad_books);
-        sad_quotes = (ImageButton) findViewById(R.id.sad_quotes);
-        sad_inspire = (ImageButton) findViewById(R.id.sad_inpire);
-        sad_travel = (ImageButton) findViewById(R.id.sad_travel);
-        sad_funny = (ImageButton) findViewById(R.id.sad_funny);
 
-        sad_puppy.setOnClickListener(this);
-        sad_music.setOnClickListener(this);
-        sad_nature.setOnClickListener(this);
-        sad_recipes.setOnClickListener(this);
-        sad_exercise.setOnClickListener(this);
-        sad_family.setOnClickListener(this);
-        sad_friends.setOnClickListener(this);
-        sad_books.setOnClickListener(this);
-        sad_quotes.setOnClickListener(this);
-        sad_inspire.setOnClickListener(this);
-        sad_travel.setOnClickListener(this);
-        sad_funny.setOnClickListener(this);
+    }
+
+    public void displayData() {
+
+        if (page == 1) {
+            setContentView(R.layout.activity_preferences);
+            nextArrow = (ImageButton) findViewById(R.id.pref1_next);
+
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            nextArrow.setOnClickListener(this);
+
+            sad_panda = (ImageButton) findViewById(R.id.pref_panda);
+            sad_music = (ImageButton) findViewById(R.id.pref_music);
+            sad_nature = (ImageButton) findViewById(R.id.pre_nature);
+            sad_recipes = (ImageButton) findViewById(R.id.pref_recipes);
+
+            sad_panda.setOnClickListener(this);
+            sad_music.setOnClickListener(this);
+            sad_nature.setOnClickListener(this);
+            sad_recipes.setOnClickListener(this);
+
+        } else if (page == 2) {
+            setContentView(R.layout.activity_preferences2);
+            nextArrow = (ImageButton) findViewById(R.id.pref2_next);
+            backArrow = (ImageButton) findViewById(R.id.pref2_back);
+
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+            nextArrow.setOnClickListener(this);
+            backArrow.setOnClickListener(this);
+
+            sad_exercise = (ImageButton) findViewById(R.id.pref_exercise);
+            sad_family = (ImageButton) findViewById(R.id.pref_fam);
+            sad_quotes = (ImageButton) findViewById(R.id.pref_quotes);
+            sad_friends = (ImageButton) findViewById(R.id.pref_friends);
+
+            sad_exercise.setOnClickListener(this);
+            sad_family.setOnClickListener(this);
+            sad_friends.setOnClickListener(this);
+            sad_quotes.setOnClickListener(this);
+
+        } else if (page == 3) {
+            setContentView(R.layout.activity_preferences3);
+            backArrow = (ImageButton) findViewById(R.id.pref3_back);
+
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+            backArrow.setOnClickListener(this);
+
+            sad_books = (ImageButton) findViewById(R.id.pref_books);
+            sad_inspire = (ImageButton) findViewById(R.id.pref_kid);
+            sad_travel = (ImageButton) findViewById(R.id.pref_travel);
+            sad_funny = (ImageButton) findViewById(R.id.pref_shark);
+
+            sad_books.setOnClickListener(this);
+            sad_inspire.setOnClickListener(this);
+            sad_travel.setOnClickListener(this);
+            sad_funny.setOnClickListener(this);
+        }
+    }
+
+    public void nextPage() {
+        System.out.println("next page clicked");
+        page = 2;
+        setContentView(R.layout.activity_preferences2);
+        displayData();
+    }
+
+    public void previousPage() {
+
     }
 
     @Override
@@ -90,54 +146,16 @@ public class SettingsMoodActivity extends AppCompatActivity implements View.OnCl
 
     @Override
     public void onClick(View view) {
-        if(view == sad_puppy){
+        if(view == sad_panda){
             String cDrawableName = "check";
             int checkID = getResources().getIdentifier(cDrawableName , "drawable",  getPackageName());
-            sad_puppy.setImageResource(checkID);
+            sad_panda.setImageResource(checkID);
         } else if (view == sad_books) {
             String cDrawableName = "check";
             int checkID = getResources().getIdentifier(cDrawableName , "drawable",  getPackageName());
             sad_books.setImageResource(checkID);
-        } else if (view == sad_exercise) {
-            String cDrawableName = "check";
-            int checkID = getResources().getIdentifier(cDrawableName , "drawable",  getPackageName());
-            sad_exercise.setImageResource(checkID);
-        } else if (view == sad_family) {
-            String cDrawableName = "check";
-            int checkID = getResources().getIdentifier(cDrawableName , "drawable",  getPackageName());
-            sad_family.setImageResource(checkID);
-        } else if (view == sad_friends) {
-            String cDrawableName = "check";
-            int checkID = getResources().getIdentifier(cDrawableName , "drawable",  getPackageName());
-            sad_friends.setImageResource(checkID);
-        } else if (view == sad_funny) {
-            String cDrawableName = "check";
-            int checkID = getResources().getIdentifier(cDrawableName , "drawable",  getPackageName());
-            sad_funny.setImageResource(checkID);
-        } else if (view == sad_inspire) {
-            String cDrawableName = "check";
-            int checkID = getResources().getIdentifier(cDrawableName , "drawable",  getPackageName());
-            sad_inspire.setImageResource(checkID);
-        } else if (view == sad_music) {
-            String cDrawableName = "check";
-            int checkID = getResources().getIdentifier(cDrawableName , "drawable",  getPackageName());
-            sad_music.setImageResource(checkID);
-        } else if (view == sad_nature) {
-            String cDrawableName = "check";
-            int checkID = getResources().getIdentifier(cDrawableName , "drawable",  getPackageName());
-            sad_nature.setImageResource(checkID);
-        } else if (view == sad_quotes) {
-            String cDrawableName = "check";
-            int checkID = getResources().getIdentifier(cDrawableName , "drawable",  getPackageName());
-            sad_quotes.setImageResource(checkID);
-        } else if (view == sad_recipes) {
-            String cDrawableName = "check";
-            int checkID = getResources().getIdentifier(cDrawableName , "drawable",  getPackageName());
-            sad_recipes.setImageResource(checkID);
-        } else if (view == sad_travel) {
-            String cDrawableName = "check";
-            int checkID = getResources().getIdentifier(cDrawableName , "drawable",  getPackageName());
-            sad_travel.setImageResource(checkID);
+        } else if (view == nextArrow) {
+            nextPage();;
         }
 
     }
