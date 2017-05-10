f6657cf84cd0637ea54b4db1b7b245848b06a6ff
Brittany Harrington <bharrin4@nd.edu>
2017-05-10 01:54:55 -0400

    Added list of youtube videos to pull from

--- app/src/main/java/Auth.java
+++
@@ -1,8 +0,0 @@
-package PACKAGE_NAME;
-
-/**
- * Created by User on 5/9/2017.
- */
-
-public class Auth {
-}
--- app/src/main/java/com/example/garrett/moodbooster/MediaYoutube.java
+++ app/src/main/java/com/example/garrett/moodbooster/MediaYoutube.java
@@ -8,6 +8,8 @@ import android.graphics.Color;
 import android.net.Uri;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import android.os.Build;
@@ -46,9 +48,10 @@ import static android.content.ContentValues.TAG;
  */
 
 public class MediaYoutube extends AppCompatActivity implements View.OnClickListener {
-    private int choice = 2;
+    private int choice;
     private int page = 2;
     private ImageButton nextArrow;
+    private TextView media_question;
 
     public String mood;
     public String antiMood;
@@ -62,6 +65,7 @@ public class MediaYoutube extends AppCompatActivity implements View.OnClickListe
     private String email;
 
     private ArrayList<String> prefs;
+    private ArrayList<String> htmls = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +74,46 @@ public class MediaYoutube extends AppCompatActivity implements View.OnClickListe
         //initializing firebase authentication object
         firebaseAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        Random rand = new Random();
+        page = rand.nextInt(2) + 1;
+
+        //store htmls of youtube videos to pull up
+        htmls.add("https://www.youtube.com/watch?v=rzhv3Q2HkbU");
+        htmls.add("https://www.youtube.com/watch?v=wNYcqj8A1Tg");
+        htmls.add("https://www.youtube.com/watch?v=r3fE6FQT82s");
+        htmls.add("https://www.youtube.com/watch?v=JD-6z3pSRs0");
+        htmls.add("https://www.youtube.com/watch?v=dEzbdLn2bJc");
+        htmls.add("https://www.youtube.com/watch?v=EFnv8az8cVE");
+        htmls.add("https://www.youtube.com/watch?v=1k_bPhglhZk");
+        htmls.add("https://www.youtube.com/watch?v=4lqZDJnOsFc");
+        htmls.add("https://www.youtube.com/watch?v=q1mAGQAw3Oc");
+        htmls.add("https://www.youtube.com/watch?v=nfWlot6h_JM&list=PLsVEeDqau8ajMwMIIDn92SdkNsSIZEU5L");
+        htmls.add("https://www.youtube.com/watch?v=QGJuMBdaqIw&index=6&list=PLsVEeDqau8ajMwMIIDn92SdkNsSIZEU5L");
+        htmls.add("https://www.youtube.com/watch?v=1k8craCGpgs&list=PLsVEeDqau8ajMwMIIDn92SdkNsSIZEU5L&index=22");
+        htmls.add("https://www.youtube.com/watch?v=gGdGFtwCNBE&index=35&list=PLsVEeDqau8ajMwMIIDn92SdkNsSIZEU5L");
+        htmls.add("https://www.youtube.com/watch?v=K0ibBPhiaG0");
+        htmls.add("https://www.youtube.com/watch?v=lp-EO5I60KA");
+        htmls.add("https://www.youtube.com/watch?v=AclhwQKlgfw");
+        htmls.add("https://www.youtube.com/watch?v=78nyMHLONqo");
+        htmls.add("https://www.youtube.com/watch?v=mX50lA_n0l0");
+        htmls.add("https://www.youtube.com/watch?v=i944XxUXkzs");
+        htmls.add("https://www.youtube.com/watch?v=-D8MOeH9oYk");
+        htmls.add("https://www.youtube.com/watch?v=dfR_LdA3fPI");
+        htmls.add("https://www.youtube.com/watch?v=6vQgHBugHxA");
+        htmls.add("https://www.youtube.com/watch?v=mNHq7T9YHHs");
+        htmls.add("https://www.youtube.com/watch?v=qWy_aOlB45Y");
+        htmls.add("https://www.youtube.com/watch?v=1919eTCoESo");
+        htmls.add("https://www.youtube.com/watch?v=NTEDDmCjd1k");
+        htmls.add("https://www.youtube.com/watch?v=bqxxtxXWMsM");
+        htmls.add("https://www.youtube.com/watch?v=mgmVOuLgFB0");
+        htmls.add("https://www.youtube.com/watch?v=g-jwWYX7Jlo");
+        htmls.add("https://www.youtube.com/watch?v=m5yCOSHeYn4");
+        htmls.add("https://www.youtube.com/watch?v=l-gQLqv9f4o");
+        htmls.add("https://www.youtube.com/watch?v=1Za8BtLgKv8");
+        htmls.add("https://www.youtube.com/watch?v=OPdbdjctx2I");
+        htmls.add("https://www.youtube.com/watch?v=2ihOXaU0I8o");
+        htmls.add("https://www.youtube.com/watch?v=Nck6BZga7TQ");
 
         //if the user is not logged in
         //that means current user will return null
@@ -87,7 +131,6 @@ public class MediaYoutube extends AppCompatActivity implements View.OnClickListe
 
         mood = getIntent().getStringExtra("mood");
         Log.d("Mood", mood);
-        System.out.println("mood= " + mood);
         prefs = new ArrayList<String>();
 
         generatePreferences();
@@ -106,7 +149,6 @@ public class MediaYoutube extends AppCompatActivity implements View.OnClickListe
 
                     for (DataSnapshot updateSnapshot : messageSnapshot.child(mood + "Settings").getChildren()) {
                         Boolean pref = (Boolean) updateSnapshot.getValue();
-                        //System.out.println("Pref: " + pref);
                         if (pref) {
                             prefs.add(updateSnapshot.getKey());
                         }
@@ -147,7 +189,7 @@ public class MediaYoutube extends AppCompatActivity implements View.OnClickListe
                         setAntiMood(anti);
                     }
 
-                    displayData();
+                    displayData(mood);
 
                 }
             }
@@ -166,9 +208,7 @@ public class MediaYoutube extends AppCompatActivity implements View.OnClickListe
         keyWord = key;
     }
 
-    public void displayData() {
-
-        System.out.println("display data");
+    public void displayData(String mood) {
 
         if (page == 1) {
             setContentView(R.layout.activity_media_youtube);
@@ -178,7 +218,10 @@ public class MediaYoutube extends AppCompatActivity implements View.OnClickListe
             setSupportActionBar(toolbar);
             nextArrow.setOnClickListener(this);
 
-            String frameVideo = "<html><body>Video from YouTube<br><iframe width=\"340\" height=\"300\" src=\"https://www.youtube.com/embed/hhoQqN9oUpo\" frameborder=\"0\" allowfullscreen></body></html>";
+            Random rand = new Random();
+            int html_index = rand.nextInt(htmls.size()) + 1;
+            String html = htmls.get(html_index);
+            String frameVideo = "<html><body>Video from YouTube<br><iframe width=\"340\" height=\"300\" src=\""+ html + "\" frameborder=\"0\" allowfullscreen></body></html>";
             WebView videoView =(WebView)findViewById(R.id.video);
             videoView.setWebViewClient(new WebViewClient(){
                 @SuppressWarnings("deprecation")
@@ -190,6 +233,9 @@ public class MediaYoutube extends AppCompatActivity implements View.OnClickListe
             WebSettings webSettings = videoView.getSettings();
             webSettings.setJavaScriptEnabled(true);
             videoView.loadData(frameVideo, "text/html", "utf-8");
+
+            media_question = (TextView) findViewById(R.id.media_question);
+            media_question.setText("Still feeling " + mood + "?");
 
         } else if (page == 2) {
             setContentView(R.layout.activity_media);
@@ -205,11 +251,12 @@ public class MediaYoutube extends AppCompatActivity implements View.OnClickListe
             webview.getSettings().setJavaScriptEnabled(true);
             webview.loadUrl(url);
 
+            media_question = (TextView) findViewById(R.id.media_question);
+            media_question.setText("Still feeling " + mood + "?");
         }
     }
 
     public void nextPage() {
-        System.out.println("next page clicked");
         Random rand = new Random();
         page = rand.nextInt(2) + 1;
         generatePreferences();
